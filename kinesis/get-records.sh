0ba05c71b9609d9c2720aa23eedacdc6c3816673
James <alphafoobar@users.noreply.github.com>
2019-03-15 13:38:41 +1300

    Use specific aws profile

--- kinesis/get-records.sh
+++ kinesis/get-records.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-stream_name=$1
-shard_array_index=${2:-0}
-shard_iterator_type=${3:-LATEST}
+profile_name=$1
+stream_name=$2
+shard_array_index=${3:-0}
+shard_iterator_type=${4:-LATEST}
 
-
-shard_id=$(aws kinesis describe-stream --stream-name ${stream_name} --query "StreamDescription.Shards[${shard_array_index}].ShardId" --output text)
-shard_iterator=$(aws kinesis get-shard-iterator --stream-name ${stream_name} --shard-id ${shard_id} --shard-iterator-type ${shard_iterator_type} --query "ShardIterator" --output text)
-aws kinesis get-records --shard-iterator ${shard_iterator}
+shard_id=$(aws kinesis describe-stream --profile ${profile_name} --stream-name ${stream_name} --query "StreamDescription.Shards[${shard_array_index}].ShardId" --output text)
+shard_iterator=$(aws kinesis get-shard-iterator --profile ${profile_name} --stream-name ${stream_name} --shard-id ${shard_id} --shard-iterator-type ${shard_iterator_type} --query "ShardIterator" --output text)
+aws kinesis get-records --limit ${limit} --shard-iterator ${shard_iterator} --profile ${profile_name}
