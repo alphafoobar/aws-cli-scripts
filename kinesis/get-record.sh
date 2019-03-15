f0fccec602e38144589684c4b760ed02e8e0db66
james <james.little@bravenewcoin.com>
2019-03-15 15:07:17 +1300

    Update script to use an amazon profile

--- kinesis/get-record.sh
+++ kinesis/get-record.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [ $# -lt 2 ]
+then
+  echo "Missing arguments. AWS profile name and kinesis stream name are required."
+  echo "Usage : $0 <aws profile name> <stream name> [shard index] [shard iterator type]"
+  exit
+fi
+
 profile_name=$1
 stream_name=$2
 shard_array_index=${3:-0}
@@ -7,4 +14,4 @@ shard_iterator_type=${4:-LATEST}
 
 shard_id=$(aws kinesis describe-stream --profile ${profile_name} --stream-name ${stream_name} --query "StreamDescription.Shards[${shard_array_index}].ShardId" --output text)
 shard_iterator=$(aws kinesis get-shard-iterator --profile ${profile_name} --stream-name ${stream_name} --shard-id ${shard_id} --shard-iterator-type ${shard_iterator_type} --query "ShardIterator" --output text)
-aws kinesis get-records --limit ${limit} --shard-iterator ${shard_iterator} --profile ${profile_name}
+aws kinesis get-records --limit 1 --shard-iterator ${shard_iterator} --profile ${profile_name}
